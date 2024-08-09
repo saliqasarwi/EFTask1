219bb35a8196c4d2250b9ea784b115d43907e661
saliq <saliq@DESKTOP-A0SOR3U>
2024-08-09 23:48:07 +0300

    Add project files.

---
+++ Context/ProductDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EFTask1.Models;
+namespace EFTask1.Context
+{
+    internal class ProductDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=.;database=my_db;Trusted_Connection=True;TrustServerCertificate=True");
+        }
+        public DbSet<Product>Products { get; set; }
+    }
+}
